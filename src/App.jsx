4fb119b39fec8a47b36207c7baa793c4399efbd1
HelenDesp <helenadespit@gmail.com>
2025-05-04 12:32:43 +0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import NFTDashboard from "./components/NFTDashboard";
 
 const config = getDefaultConfig({
   appName: "Re1",
-  projectId: "4b756b2f8d1d6da3fdbb0254c34c8a22", // Replace with your real WalletConnect Project ID
+  projectId: "4a489f38e57beea61afffc11e3d21ffe", // Replace with your real WalletConnect Project ID
   chains: [base],
   transports: {
     [base.id]: http(),
