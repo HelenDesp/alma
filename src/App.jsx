ab650de6a84dde72c023f92c7f4599a747cd2de1
HelenDesp <helenadespit@gmail.com>
2025-05-04 11:28:33 +0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,8 @@ import "@rainbow-me/rainbowkit/styles.css";
 import NFTDashboard from "./components/NFTDashboard";
 
 const config = getDefaultConfig({
-  appName: "ReVerse Genesis",
-  projectId: "abc123xyz_placeholder", // Replace with your real WalletConnect Project ID
+  appName: "Re1",
+  projectId: "4b756b2f8d1d6da3fdbb0254c34c8a22", // Replace with your real WalletConnect Project ID
   chains: [base],
   transports: {
     [base.id]: http(),
